7bf5c0f45c934b20309143d018c2bdfe73fb2aff
Angga Deongs <anggadeongs11@gmail.com>
2022-09-14 12:21:12 +0700

    update

--- src/scripts/components/Home.js
+++ src/scripts/components/Home.js
@@ -52,6 +52,7 @@ const Home = (() => {
           .addClass("active")
           .siblings(".home__sub-category__item")
           .removeClass("active");
+        _parents.removeClass("show-dropdown");
         e.stopPropagation();
       });
     }
@@ -62,25 +63,55 @@ const Home = (() => {
     $(".js-home-sub-category .home__sub-category__item").on("click", (e) => {
       const _this = $(e.currentTarget);
       const _category = _this.attr("data-id");
+      const _categoryLink = _this.attr("data-link");
       const _str = "&category=" + _category + "&action=dropdown_ajax";
       $.ajax({
         type: "POST",
         dataType: "html",
         url: ajax_posts.ajaxurl,
         data: _str,
+        beforeSend: () => {
+          _this
+            .parents(".home__posts")
+            .find(".card-primary")
+            .each((i, el) => {
+              $(el).addClass("is-hidden");
+            });
+          _this
+            .parents(".home__posts")
+            .find(".home__action")
+            .addClass("is-hidden");
+          _this
+            .parents(".home__posts")
+            .find(".home__loading")
+            .addClass("is-visible");
+        },
         success: (data) => {
           const _data = $(data);
           _this.parents(".home__posts").find(".home__list").html(_data);
-          setTimeout(() => {
-            if ($(".card-primary__img__el").length) {
-              $(".card-primary__img__el").each((i, el) => {
-                $(el).imagesLoaded(() => {
-                  if (!$(el).parents(".card-primary").hasClass("is-visible")) {
-                    $(el).parents(".card-primary").addClass("is-visible");
-                  }
-                });
+          if ($(".card-primary__img__el").length) {
+            $(".card-primary__img__el").each((i, el) => {
+              $(el).imagesLoaded(() => {
+                if (!$(el).parents(".card-primary").hasClass("is-visible")) {
+                  $(el).parents(".card-primary").addClass("is-visible");
+                }
               });
-            }
+            });
+          }
+          _this
+            .parents(".home__posts")
+            .find(".home__loading")
+            .removeClass("is-visible");
+          setTimeout(() => {
+            _this
+              .parents(".home__posts")
+              .find(".card-primary")
+              .removeClass("is-hidden");
+            _this
+              .parents(".home__posts")
+              .find(".home__action")
+              .removeClass("is-hidden")
+              .attr("href", _categoryLink);
           }, 100);
         },
         error: (data) => {},
